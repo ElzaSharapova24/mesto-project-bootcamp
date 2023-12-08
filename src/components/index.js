ccd18f357ebe16d831b37b4fe95c2ef3644775a1
Elza Sharapova <elzananakidonskaya@yandex.ru>
2023-12-08 13:00:27 +0400

    refactor:добавление валидации форм

--- src/components/index.js
+++ src/components/index.js
@@ -1,16 +1,23 @@
+import '../styles/index.css';
+import { initialCards } from './data';
+
+
 document.addEventListener("DOMContentLoaded", function () {
   const modalEditElement = document.querySelector(".modal_type_edit");
   const modalAddElement = document.querySelector(".modal_type_add");
   const btnEditModal = document.querySelector(".profile__btn");
   const btnAddModal = document.querySelector(".profile__button");
   const modalGalleryElement = document.querySelector(".modal_type_img");
-  const modalCloseGallery = modalGalleryElement.querySelector(".modal__btn-close");
+  const modalCloseGallery =
+    modalGalleryElement.querySelector(".modal__btn-close");
   const formElement = document.querySelector(".modal__form");
-  const form = document.querySelector(".modal__form-input");
+  const form = document.querySelector(".modal__form-add");
   const inputName = document.querySelector(".modal__input-avatar-name");
   const inputDescr = document.querySelector(".modal__input-avatar-descr");
   const profileName = document.querySelector(".profile__avatar-name");
-  const profileDescription = document.querySelector(".profile__avatar-description");
+  const profileDescription = document.querySelector(
+    ".profile__avatar-description"
+  );
 
   //открытие модального окна
 
@@ -24,27 +31,26 @@ document.addEventListener("DOMContentLoaded", function () {
     modal.classList.remove("modal__is-opened");
   }
 
-  function closeModalClick(e) {
+  function closeModalClick(evt) {
     if (
-      e.target === e.currentTarget ||
-      e.target.classList.contains("modal__btn-close")
+      evt.target === evt.currentTarget ||
+      evt.target.classList.contains("modal__btn-close")
     ) {
       const activeModal = document.querySelector(".modal.modal__is-opened");
       closeModalWindow(activeModal);
     }
   }
 
-  function handleFormEditSubmit(e) {
-    e.preventDefault();
+  function handleFormEditSubmit(evt) {
+    evt.preventDefault();
 
     const nameValue = inputName.value;
     const descrValue = inputDescr.value;
 
     profileName.textContent = nameValue;
     profileDescription.textContent = descrValue;
-    closeModalClick(e);
+    closeModalClick(evt);
   }
-
 
   function handleOpenForm(evt) {
     evt.preventDefault();
@@ -65,11 +71,13 @@ document.addEventListener("DOMContentLoaded", function () {
   formElement.addEventListener("submit", handleFormEditSubmit);
 
   btnEditModal.addEventListener("click", function () {
-      inputName.value = profileName.innerText;
-      inputDescr.value = profileDescription.innerText;
-      openModalWindow(modalEditElement)
+    inputName.value = profileName.innerText;
+    inputDescr.value = profileDescription.innerText;
+    openModalWindow(modalEditElement);
   });
-  modalCloseGallery.addEventListener("click", () => closeModalClick(modalGalleryElement));
+  modalCloseGallery.addEventListener("click", () =>
+    closeModalClick(modalGalleryElement)
+  );
   btnAddModal.addEventListener("click", () => openModalWindow(modalAddElement));
   modalEditElement.addEventListener("mousedown", closeModalClick);
   modalAddElement.addEventListener("mousedown", closeModalClick);
@@ -95,8 +103,10 @@ document.addEventListener("DOMContentLoaded", function () {
       .querySelector(".picture")
       .cloneNode(true);
 
-    const picturesCardImg = pictureElement.querySelector('.picture__card-img');
-    const picturesCardTitle = pictureElement.querySelector('.picture__card-title');
+    const picturesCardImg = pictureElement.querySelector(".picture__card-img");
+    const picturesCardTitle = pictureElement.querySelector(
+      ".picture__card-title"
+    );
 
     picturesCardTitle.textContent = name;
     picturesCardImg.src = link;
@@ -113,8 +123,8 @@ document.addEventListener("DOMContentLoaded", function () {
       modalImage.src = link;
       modalImage.alt = name;
       modalCaption.innerHTML = name;
-      openModalWindow(modalGalleryElement)
-  });
+      openModalWindow(modalGalleryElement);
+    });
 
     btnDeleteCard.addEventListener("click", () => pictureElement.remove());
 
@@ -125,4 +135,88 @@ document.addEventListener("DOMContentLoaded", function () {
     initialCards.forEach(renderCard);
   }
   renderInitialCards();
+
+  // validation
+
+  function enableValidationForm(config) {
+    const formList = document.querySelectorAll(
+      config.formSelector
+    );
+
+    formList.forEach(function (formItem) {
+      setEventListener(formItem, config);
+    });
+  }
+  enableValidationForm({
+    formSelector: ".modal__form",
+    inputSelector: ".modal__input",
+    submitButtonSelector: ".modal__btn",
+    inactiveButtonClass: "modal__btn-disabled",
+    inputErrorClass: "modal__input_type_error",
+  });
+
+  function setEventListener(
+    formItem,
+    config,
+  ) {
+    const inputList = formItem.querySelectorAll(
+      config.inputSelector
+    );
+    const btnSubmit = formItem.querySelector(
+      config.submitButtonSelector
+    );
+    disabledButton(
+      btnSubmit,
+      formItem.checkValidity(),
+      config,
+    );
+
+    inputList.forEach(function (inputItem) {
+      inputItem.addEventListener("input", function () {
+        checkValidity(inputItem, formItem, config);
+        disabledButton(
+          btnSubmit,
+          formItem.checkValidity(),
+          config,
+        );
+      });
+
+      formItem.addEventListener("submit", function (evt) {
+        evt.preventDefault();
+      });
+    });
+  }
+
+  function checkValidity(inputItem, formItem, config) {
+    const isInputValidity = inputItem.validity.valid;
+    const formErrorMessage = formItem.querySelector(`#${inputItem.name}-error`);
+
+    if (!isInputValidity) {
+      showInputError(inputItem, formErrorMessage, config);
+    } else {
+      hideInputError(inputItem, formErrorMessage, config);
+    }
+  }
+
+  function disabledButton(btnElement, isActive, config) {
+    if (isActive) {
+      console.log(isActive);
+      btnElement.disabled = false;
+      btnElement.classList.remove(config.inactiveButtonClass);
+    } else {
+      btnElement.disabled = true;
+      btnElement.classList.add(config.inactiveButtonClass);
+    }
+  }
+
+  function showInputError(inputItem, formErrorMessage, config) {
+    inputItem.classList.add(config.inputErrorClass);
+    formErrorMessage.textContent = inputItem.validationMessage;
+  }
+
+  function hideInputError(inputItem, formErrorMessage, config) {
+    inputItem.classList.remove(config.inputErrorClass);
+    formErrorMessage.textContent = inputItem.validationMessage;
+  }
+
 });
